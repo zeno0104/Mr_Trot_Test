7568076d0c2d80992445e8ff62fa12812a0a81b5
zenoo97 <jaehun4086@naver.com>
2025-03-03 14:22:00 +0900

    [Update] Footer 작업

--- MrTrot3/src/App.tsx
+++ MrTrot3/src/App.tsx
@@ -2,13 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home";
 import { Test } from "./pages/Test";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/test" element={<Test />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
---
+++ MrTrot3/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="Footer">
+      <p>2025 Jaehoon-Ahn. All rights reserved.</p>
+      <p>jaehun4086@naver.com</p>
+    </div>
+  );
+};
